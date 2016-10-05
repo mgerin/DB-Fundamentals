30a1f05d642b8a7a0d875bb02d347fda55466a5d
Marin Gerin <marin_gerin@abv.bg>
2016-10-05 17:16:28 +0300

    New tasks

--- HOMEWORK.sql
+++ HOMEWORK.sql
@@ -80,7 +80,7 @@ INSERT INTO Users VALUES ('mgerin', '2131321432423', NULL, '2016-10-02', 'false'
 --PROBLEM 9
 
 ALTER TABLE Users
-DROP CONSTRAINT PK__Users__3214EC072879A487;
+DROP CONSTRAINT PK__Users__3214EC07F77747D8;
 
 ALTER TABLE Users
 ADD CONSTRAINT PK_Users PRIMARY KEY (Id, Username);
@@ -177,3 +177,177 @@ VALUES (1, 'FILM ONE', 2, 2016, 120, 1, 3, NULL, 'NOTES'),
 
 --PROBLEM 14
 
+CREATE DATABASE CarRental;
+
+USE CarRental
+GO
+
+CREATE TABLE Categories
+(
+Id INT NOT NULL PRIMARY KEY,
+Category NVARCHAR(50) NOT NULL UNIQUE,
+DailyRate FLOAT(1) NOT NULL,
+WeeklyRate FLOAT(1) NOT NULL,
+MonthlyRate FLOAT(1) NOT NULL,
+WeekendRate FLOAT(1) NOT NULL
+)
+
+CREATE TABLE Cars
+(
+Id INT NOT NULL PRIMARY KEY,
+PlateNumber NVARCHAR(15) NOT NULL,
+Make NVARCHAR(30) NOT NULL,
+Model NVARCHAR(30) NOT NULL,
+CarYear INT NOT NULL,
+CategoryId INT,
+Doors INT,
+Picture VARBINARY(max),
+Condition NVARCHAR(50),
+Available BIT
+)
+
+CREATE TABLE Employees
+(
+Id INT NOT NULL PRIMARY KEY,
+FirstName NVARCHAR(50) NOT NULL,
+LastName NVARCHAR(50) NOT NULL,
+Title NVARCHAR(50),
+Notes NVARCHAR(max)
+)
+
+CREATE TABLE Customers
+(
+Id INT NOT NULL PRIMARY KEY,
+DriverLicenceNumber NVARCHAR(20) NOT NULL,
+FullName NVARCHAR(150) NOT NULL,
+Address NVARCHAR(150) NOT NULL,
+City NVARCHAR(50) NOT NULL,
+ZIPCode NVARCHAR(10),
+Notes NVARCHAR(max)
+)
+
+CREATE TABLE RentalOrders
+(
+Id INT NOT NULL PRIMARY KEY,
+EmployeeId INT NOT NULL,
+CustomerId INT NOT NULL,
+CarId INT NOT NULL,
+CarCondition NVARCHAR(50),
+TankLevel INT NOT NULL,
+KilometrageStart FLOAT(1) NOT NULL,
+KilometrageEnd FLOAT(1) NOT NULL,
+TotalKilometrage FLOAT(1) NOT NULL,
+StartDate DATE NOT NULL,
+EndDate DATE NOT NULL,
+TotalDays INT NOT NULL,
+RateApplied FLOAT(1) NOT NULL,
+TaxRate FLOAT(1) NOT NULL,
+OrderStatus NVARCHAR(30) NOT NULL,
+Notes NVARCHAR(max)
+)
+
+INSERT INTO Categories VALUES(1, 'Category 1', 13.55, 60, 220.5, 35),
+(2, 'Category 2', 20.55, 70, 250.5, 50),
+(3, 'Category 3', 25.55, 80, 320.5, 65);
+
+INSERT INTO Cars VALUES (1, 'CA4223XP', 'BMW', '320d', 2001, 1, 4, NULL, 'GOOD', 'TRUE'),
+(2, 'CB2223XP', 'OPEL', 'VECTRA', 2005, 2, 5, NULL, 'GOOD', 'TRUE'),
+(3, 'PA4265AP', 'VW', 'PASSAT', 2010, 3, 4, NULL, 'PERFECT', 'TRUE');
+
+INSERT INTO Employees VALUES (1, 'IVAN', 'IVANOV', 'JOHNY', 'NOTES'),
+(2, 'STAMAT', 'PETROV', 'SP', 'NOTES SP'),
+(3, 'KOSTADIN', 'IVANOV', 'KOKI', NULL);
+
+INSERT INTO Customers VALUES (1, '34234234SFSD', 'BOBI B', 'RICCARDO VACCARINI', 'SOFIA', '1404', 'Notes'),
+(2, '34SSDASAS234234SFSD', 'MANO', 'ANGEL KANCHEV', 'SOFIA', '1404', 'Notes'),
+(3, '34234234SDADASDFSD', 'GESHEV', 'GOCE DELCHEV', 'SOFIA', '1000', NULL);
+
+INSERT INTO RentalOrders VALUES (1, 2, 1, 3, 'GOOD', 23, 140440, 140640, 200,
+ '2016-09-08', '2016-09-08', 1, 13.55, 20, 'CONFIRMED', 'Notes'),
+ (2, 2, 1, 3, 'GOOD', 23, 140440, 140640, 200,
+ '2016-09-08', '2016-09-08', 1, 13.55, 20, 'CONFIRMED', 'Notes'),
+ (3, 2, 1, 3, 'GOOD', 23, 140440, 140640, 200,
+ '2016-09-08', '2016-09-08', 1, 13.55, 20, 'CONFIRMED', 'Notes');
+
+ --PROBLEM 15
+
+ CREATE DATABASE Hotel;
+ 
+ USE Hotel
+ GO
+
+ CREATE TABLE Employees
+ (
+ Id,
+ FirstName,
+ LastName,
+ Title,
+ Notes
+ );
+
+ CREATE TABLE Customers
+ (
+ AccountNumber,
+ FirstName,
+ LastName, 
+ PhoneNumber, 
+ EmergencyName, 
+ EmergencyNumber, 
+ Notes
+ );
+
+ CREATE TABLE RoomStatus
+ (
+ RoomStatus, 
+ Notes
+ );
+
+ CREATE TABLE RoomTypes
+ (
+ RoomType, 
+ Notes
+ );
+
+ CREATE TABLE BedTypes
+ (
+ BedType, 
+ Notes
+ );
+
+ CREATE TABLE Rooms
+ (
+ RoomNumber, 
+ RoomType,
+ BedType, 
+ Rate, 
+ RoomStatus, 
+ Notes
+ );
+
+ CREATE TABLE Payments
+ (
+ Id, 
+ EmployeeId, 
+ PaymentDate, 
+ AccountNumber, 
+ FirstDateOccupied, 
+ LastDateOccupied, 
+ TotalDays, 
+ AmountCharged, 
+ TaxRate, 
+ TaxAmount, 
+ PaymentTotal, 
+ Notes
+ );
+
+ CREATE TABLE Occupancies
+ (
+ Id, 
+ EmployeeId, 
+ DateOccupied, 
+ AccountNumber, 
+ RoomNumber, 
+ RateApplied, 
+ PhoneCharge, 
+ Notes
+ );
