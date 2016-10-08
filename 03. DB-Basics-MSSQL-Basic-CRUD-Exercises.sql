3e549710dd88797ed47aac3a86b6c53e28dab149
Marin Gerin <marin_gerin@abv.bg>
2016-10-09 01:20:24 +0300

    CRUD-Exercises

---
+++ 03. DB-Basics-MSSQL-Basic-CRUD-Exercises.sql
@@ -0,0 +1,25 @@
+--PROBLEM 01
+--IMPORT DATABASES
+
+--PROBLEM 02
+
+SELECT * FROM Departments;
+
+--PROBLEM 03
+
+SELECT Name FROM Departments;
+
+--PROBLEM 04
+
+SELECT FirstName, LastName, Salary FROM Employees;
+
+--PROBLEM 05
+
+SELECT FirstName, MiddleName, LastName FROM Employees;
+
+--PROBLEM 06
+
+SELECT E.FirstName + '.' + E.LastName + '@softuni.bg' AS 'Full Email Address' FROM Employees AS E;
+
+--PROBLEM 07
+
