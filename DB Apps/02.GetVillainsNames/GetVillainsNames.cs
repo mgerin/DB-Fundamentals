ca6329b11f1dcf79d64f89617797c32b0d8716b6
Marin Gerin <marin_gerin@abv.bg>
2016-10-29 00:25:39 +0300

    New stuff

--- DB Apps/02.GetVillainsNames/GetVillainsNames.cs
+++ DB Apps/02.GetVillainsNames/GetVillainsNames.cs
@@ -1,10 +1,26 @@
-﻿namespace _02.GetVillainsNames
+﻿using System;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _02.GetVillainsNames
 {
     class GetVillainsNames
     {
+        public static SqlConnection Connection = new SqlConnection("Data Source=(local); Initial Catalog=MinionsDB;Integrated Security=True");
         static void Main()
         {
-
+            SqlCommand command = new SqlCommand("SELECT v.Name, COUNT(MinionId) AS c " + "FROM Villains v "
+                + "JOIN MinionsVillains mv ON v.Id = mv.VillainId " + "GROUP BY v.Name " 
+                + "HAVING COUNT(MinionId) > 3 " + "ORDER BY c DESC", Connection);
+            Connection.Open();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0] + " " + reader[1]);
+                }
+            }
+            Connection.Close();
         }
     }
 }
---
+++ DB Apps/03.GetMinionNames/GetMinionNames.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _03.GetMinionNames
+{
+    class GetMinionNames
+    {
+        public static SqlConnection Connection = new SqlConnection("Data Source=(local);Initial Catalog=MinionsDB;Integrated Security=True");
+        static void Main()
+        {
+            int id = int.Parse(Console.ReadLine());
+            string villainNameSQL = "SELECT Name FROM Villains WHERE id = @villainId";
+            SqlCommand command = new SqlCommand(villainNameSQL, Connection);
+            command.Parameters.AddWithValue("@villainId", id);
+            Connection.Open();
+            using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.SingleResult))
+            {
+                if (!reader.HasRows)
+                {
+                    Console.WriteLine("No villain with ID " + id + " exists in the database.");
+                    return;
+                }
+                reader.Read();
+                string villainName = reader[0].ToString();
+                Console.WriteLine("Villain: " + villainName);
+    
+            }
+            Connection.Close();
+
+            string minionsSQL = "SELECT m.Name, Age " + "FROM Villains v " +
+                                "JOIN MinionsVillains mv ON v.Id=mv.VillainId "
+                                + "JOIN Minions m ON m.Id=mv.MinionId " + "WHERE v.Id = @villainId";
+            command = new SqlCommand(minionsSQL, Connection);
+            command.Parameters.AddWithValue("@villainId", id);
+            Connection.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader() )
+            {
+                if (!reader.HasRows)
+                {
+                    Console.WriteLine("<no minions>");
+                    return;
+                }
+
+                int counter = 1;
+                while (reader.Read())
+                {
+                    Console.WriteLine(counter + " " + reader["name"] + " " + reader["age"]);
+                    counter++;
+                }
+            }
+
+            Connection.Close();
+        }
+    }
+}
