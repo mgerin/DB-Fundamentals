8b63d214458dcbc9c55d42bd3c79ed4d145ce191
mgerin <marin_gerin@abv.bg>
2016-11-10 21:12:31 +0200

    New stuff

---
+++ MiniORM/MiniORM/EntityManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using CustomORM.Core;
+
+namespace MiniORM
+{
+    class EntityManager : IDbContext
+    {
+        private SqlConnection connection;
+        private string connectionString;
+        private bool isCodeFirst;
+
+        public EntityManager(string connectionString, bool isCodeFirst)
+        {
+            this.connectionString = connectionString;
+            this.isCodeFirst = isCodeFirst;
+        }
+
+        public bool Persist(object entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T FindById<T>(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAll<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAll<T>(string @where)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T FindFirst<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T FindFirst<T>(string @where)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete<T>(object entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteById<T>(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
