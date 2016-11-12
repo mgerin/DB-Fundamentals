095bddd6c9a06ffefcd882589c01c5eda91df93a
Marin Gerin <marin_gerin@abv.bg>
2016-11-13 00:06:19 +0200

    New stuff

--- MiniORM/MiniORM/EntityManager.cs
+++ MiniORM/MiniORM/EntityManager.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 using CustomORM.Core;
+using MiniORM.Attributes;
 
 namespace MiniORM
 {
@@ -18,7 +23,110 @@ namespace MiniORM
 
         public bool Persist(object entity)
         {
-            throw new System.NotImplementedException();
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Cannot persist null entity");
+            }
+
+            if (isCodeFirst && !CheckIfTableExists(entity.GetType()))
+            {
+                this.CreateTable(entity.GetType());
+            }
+
+            Type entityType = entity.GetType();
+            FieldInfo idInfo = GetId(entityType);
+            int id = (int) idInfo.GetValue(entity);
+
+            if (id <= 0)
+            {
+                return this.Insert(entityType, idInfo);
+            }
+
+            return this.Update(entityType, idInfo);
+        }
+
+        private bool Update(Type entityType, FieldInfo idInfo)
+        {
+            int numberOfAffectedRows = 0;
+            //TODO: code
+
+            return numberOfAffectedRows > 0;
+        }
+
+        private bool Insert(Type entityType, FieldInfo idInfo)
+        {
+            int numberOfAffectedRows = 0;
+
+
+            return numberOfAffectedRows > 0;
+        }
+
+        private void CreateTable(Type entity)
+        {
+            string creationString = PrepareTableCreationString(entity);
+            using (connection = new SqlConnection(this.connectionString))
+            {
+                this.connection.Open();
+                SqlCommand command = new SqlCommand(creationString, this.connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private string PrepareTableCreationString(Type entity)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"CREATE TABLE {GetTableName(entity)} (");
+            builder.Append($"Id INT PRIMARY KEY IDENTITY(1,1), ");
+
+            FieldInfo[] columnsInfos =
+                entity.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                .Where(x => x.IsDefined(typeof(ColumnAttribute)))
+                .ToArray();
+
+            foreach (FieldInfo columnField in columnsInfos)
+            {
+                builder.Append($"{this.GetColumnName(columnField)} {this.GetTypeToDB(columnField)}, ");
+            }
+            builder.Remove(builder.Length - 2, 2);
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+
+        private string GetTypeToDB(FieldInfo field)
+        {
+            switch (field.FieldType.Name)
+            {
+                case "Int32":
+                    return "int";
+                case "String":
+                    return "varchar(max)";
+                case "DateTime":
+                    return "datetime";
+                case "Boolean":
+                    return "bit";
+                default:
+                    Console.WriteLine(field.FieldType.Name);
+                    throw new ArgumentException("No such present type - try extending the framework!");
+            }
+        }
+
+        private bool CheckIfTableExists(Type type)
+        {
+            string query =
+                $"SELECT COUNT(name) " +
+                $"FROM sys.sysobjects " +
+                $"WHERE [Name] = '{this.GetTableName(type)}' AND [xtype] = 'U'";
+
+            int numberOfTables = 0;
+            using (connection = new SqlConnection(this.connectionString))
+            {
+                this.connection.Open();
+                SqlCommand command = new SqlCommand(query, this.connection);
+                numberOfTables = (int) command.ExecuteScalar();
+            }
+
+            return numberOfTables > 0;
         }
 
         public T FindById<T>(int id)
@@ -54,6 +162,68 @@ namespace MiniORM
         public void DeleteById<T>(int id)
         {
             throw new System.NotImplementedException();
+        }
+
+        private FieldInfo GetId(Type entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Cannot get id for null type");
+            }
+
+            FieldInfo id =
+                entity.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                    .FirstOrDefault(x => x.IsDefined(typeof(IdAttribute)));
+
+            if (id == null)
+            {
+                throw new ArgumentNullException("No id field was found the current class!");
+            }
+
+            return id;
+        }
+
+        private string GetTableName(Type entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Table null!");
+            }
+
+            if (!entity.IsDefined(typeof(EntityAttribute)))
+            {
+                throw new ArgumentException("Cannot get table name of entity!");
+            }
+
+            string tableName = entity.GetCustomAttribute<EntityAttribute>().TableName;
+
+            if (tableName == null)
+            {
+                throw new ArgumentNullException("Table name cannot be null!");
+            }
+
+            return tableName;
+        }
+
+        private string GetColumnName(FieldInfo field)
+        {
+            if (field == null)
+            {
+                throw new ArgumentNullException("Field cannot be null");
+            }
+
+            if (!field.IsDefined(typeof(ColumnAttribute)))
+            {
+                return field.Name;
+            }
+
+            string columnName = field.GetCustomAttribute<ColumnAttribute>().ColumnName;
+            if (columnName == null)
+            {
+                throw new ArgumentNullException("Column name cannot be null!");
+            }
+
+            return columnName;
         }
     }
 }
